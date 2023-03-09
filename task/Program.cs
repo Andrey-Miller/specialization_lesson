63422822cb7b2feb45a6b29fcd42658f5d4b2041
sinedd <sineddam@mail.ru>
2023-03-09 19:07:11 +0500

    code edit

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ArrayModify(string[] array)
+{
+    int newArrLength = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) { newArrLength++; }
+    }
+    string[] newArray = new string[newArrLength];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+string[] array = { "hello", "cat", "   ", "world", "^_^", "123", "-10", "----" };
+string[] newArray = ArrayModify(array);
+System.Console.WriteLine($"Исходный массив: [{string.Join(",", array)}]");
+System.Console.WriteLine($"Новый массив: [{string.Join(",", newArray)}]");
